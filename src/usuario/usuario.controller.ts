a0cae224e41e60acf7d3de8c5e10f72533b56ee5
Heitor-Grande <heitorogrande2001@gmail.com>
2025-01-23 21:45:25 -0300

    comentando funções

--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -4,10 +4,12 @@ import { UsuarioService } from './usuario.service';
 @Controller('usuario')
 export class UsuarioController {
     constructor(private readonly usuarioService: UsuarioService) { }
+    //carrega infos do usuario
     @Get("/carregar/usuario/:id_usuario")
     carregarUsuario(@Param() params: any) {
         return this.usuarioService.carregarUsuarioById(params)
     }
+    //atualiza conta do usuario
     @Put("/atualizar/minha/conta/:id_usuario")
     atualiarUsuario(@Param() params: any, @Body() body: any) {
         return this.usuarioService.atualizarUsuarioById(params, body)
